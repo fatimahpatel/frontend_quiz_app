77363bf2f9ee63387747cc2f34c09415347f1155
Fatimah Patel <fatimahpatel10@gmail.com>
2023-09-01 20:49:12 +0100

    new quizData array with three objects

---
+++ script.js
@@ -0,0 +1,27 @@
+const quizData = [
+    {
+        question: "What does JSON stand for?",
+        a: "Javascript Object Notation",
+        b: "Javascript Orientation Notation",
+        c: "Javascript Object Notary",
+        d: "Java Sequence Orientation Notation",
+        correct: "a",
+    },
+    {
+        question: "What can be used to style a webpage?",
+        a: "Javascipt",
+        b: "HTML",
+        c: "CSS",
+        d: "Java",
+        correct: "c",
+    },
+
+    {question: "Which of these is not a HTML tag?" ,
+        a: "<div>",
+        b: "<input>",
+        c: "<label>",
+        d: "<stop>",
+        correct: "d",
+    
+    }    
+]
