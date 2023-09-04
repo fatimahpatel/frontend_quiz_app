17013c21fbff69487b95c9ce7c19d8a37d22c7d1
Fatimah Patel <fatimahpatel10@gmail.com>
2023-09-04 20:30:33 +0100

    get submit button from Document

--- script.js
+++ script.js
@@ -33,3 +33,4 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitButton = document.getElementById('submit');
