1055b1a2cbf7ab96f34908ae173da5c24cfc0fe5
Fatimah Patel <fatimahpatel10@gmail.com>
2023-09-12 20:54:28 +0100

    passed in answerElement to correctAnswer

--- script.js
+++ script.js
@@ -65,15 +65,17 @@ function deselectAnswers() {
 //then we need to match that with the correct answer
 
 function correctAnswer(answerElement){
-    if (answerElement.id == quizData[currentQuiz].correct){
-        // score++;
-        console.log("correct");
-        // console.log(score);
-    }
-    else{
-        console.log("incorrect")
-        console.log(answerElement.id)
-    }
+    console.log(answerElement)
+    console.log(quizData[currentQuiz].correct)
+    // if (answerElement.id == quizData[currentQuiz].correct){
+    //     // score++;
+    //     console.log("correct");
+    //     // console.log(score);
+    // }
+    // else{
+    //     console.log("incorrect")
+    //     console.log(answerElement.id)
+    // }
 }
 
 //when button is clicked DONE
@@ -85,7 +87,7 @@ submitButton.addEventListener('click', () =>{
     
     answerElements.forEach((answerElement) => {
         if (answerElement.checked){
-            correctAnswer()
+            correctAnswer(answerElement)
         }
     })}
 )
